0b0c5251777f454e67a7d29a17d1d7db41245182
rjhale1971 <rjhale@northrivergeographic.com>
2022-02-13 20:45:58 -0500

    updating map

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,8 +1,10 @@
 #!/bin/bash 
 
-wget -O ../data/hydrant.osm --post-file=hydrant.osm "https://overpass-api.de/api/interpreter"
+wget -O ../data/hydrant.osm --post-file=hydrant_request.osm "https://overpass-api.de/api/interpreter"
 
-ogr2ogr -f "GEOJSON" ../data/hydrant.geojson ../data/hydrant.osm points
+ogr2ogr -f "GEOJSON" ../data/hydrant_conv.geojson ../data/hydrant.osm points
+
+ogr2ogr -clipsrc ../data/tn.geojson ../data/hydrant.geojson ../data/hydrant_conv.geojson
 
 var="var hydrant = {"
 sed -i "1s/.*/$var/" ../data/hydrant.geojson 
